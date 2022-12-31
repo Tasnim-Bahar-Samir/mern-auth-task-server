b31ab1bd7b9fd473c65d06bda2937a29434b3c6a
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-12-31 19:06:15 +0600

    set api's for login and register

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const bcrypt = require('bcryptjs')
+const app = express();
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 app.use(cors())
@@ -12,8 +14,63 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.skbfv9j.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const userCollection = client.db('dbUser1').collection('users')
 
+async function run(){
+    try{
+        app.post("/users", async (req, res) => {
 
+            const {name,email,password,address,phone} = req.body;
+            const ancryptedPassword = await bcrypt.hash(password,10)
+            console.log(ancryptedPassword)
+            const query = {email}
+            const alreadyAddedUser = await userCollection.find(query).toArray();
+            if(alreadyAddedUser.length){
+              return res.send({success:false, message:"This user already exist"})
+            }
+            const user = {
+              name,
+              email,
+              password: ancryptedPassword,
+              email,
+              address,
+              phone,
+            }
+            const result = await userCollection.insertOne(user);
+      
+            if (result.insertedId) {
+              res.send({
+                success: true,
+                message: "User registered successfully",
+              });
+            } else {
+              res.send({
+                success: false,
+                message: "Failed to register",
+              });
+            }
+          });
+
+    app.post('/login', async(req,res)=>{
+      const {email,password} = req.body;
+      const user = await userCollection.findOne({email})
+      if(!user){
+        return res.send({success: false, message:"No user found.Please register first."})
+      }
+      if(await bcrypt.compare(password, user.password)){
+        const token = jwt.sign({email},process.env.JWT_SECRETEKEY)
+        if(res.status(201)){
+          res.send({ success:true, message:'Token given'})
+        }
+      }else{
+        res.send({ success:false, message:'Failed to generate token'})
+      }
+    })
+    }catch(err){
+        console.log(err.message,err.name)
+    }
+}
+run()
 
 app.get('/',(req,res)=>{
     res.send('Server is running')
